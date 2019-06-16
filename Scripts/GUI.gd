770f2c94c6a2817c22269bae4abe1419e4d74a64
Felipe <felipe@Ubuntu18.04>
2019-06-15 21:43:19 -0300

    Adicionado os comentários nos scriptes do tutorial

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -1,5 +1,6 @@
 extends Control
 
+# Lista dos endereços das texturas dos botões
 var icons_butons = [
 	"res://Sprites/plus_white.png", 
 	"res://Sprites/plus_orange.png", 
@@ -11,34 +12,31 @@ var icons_butons = [
 func _ready():
 	pass
 
-
+# Método de ação do botão mais
 func _on_btn_plus_pressed():
-	save_system.contador += 1
-	$menu/central_button/btn_plus.icon = load(icons_butons[1])
-	$menu/central_button/btn_plus/time_recovery_icon.start()
+	save_system.contador += 1 # Incrementa o contador 
+	$menu/central_button/btn_plus.icon = load(icons_butons[1]) # Carrega a textura do icone do botão
+	$menu/central_button/btn_plus/time_recovery_icon.start() # inicia o contador para restaurar a textura do botão
 
-
+# Método de ação do botão menos
 func _on_btn_menos_pressed():
-	save_system.contador -= 1
-	$menu/central_button/btn_menos.icon = load(icons_butons[3])
-	$menu/central_button/btn_menos/timer_recovery_icon.start()
+	save_system.contador -= 1 # Decrementa o contador 
+	$menu/central_button/btn_menos.icon = load(icons_butons[3]) # Carrega a textura do icone do botão
+	$menu/central_button/btn_menos/timer_recovery_icon.start() # inicia o contador para restaurar a textura do botão
 
-
-
-
-
+# Recovery da textura do botão 
 func _on_time_recovery_icon_timeout():
 	$menu/central_button/btn_plus.icon = load(icons_butons[0])
 
-
+# Recovery da textura do botão 
 func _on_timer_recovery_icon_menos_timeout():
 	$menu/central_button/btn_menos.icon = load(icons_butons[2])
 
-
+# Ação de salvamento 
 func _on_btn_save_pressed():
 	save_system.save()
 
-
+# Ação de carregamento e atualização do node display_message
 func _on_btn_load_pressed():
 	save_system._load()
 	$menu/display_message.text = str(save_system.contador)
--- Scripts/save_system.gd
+++ Scripts/save_system.gd
@@ -5,25 +5,28 @@ var contador = 0 setget set_contador
 
 signal update_display_message
 
+# Encapsulamento utilizanodo método setget
 func set_contador(valor):
 	contador = valor 
 	emit_signal("update_display_message")
 
+# Método de salvamento do contador 
 func save():
-	var arquivo = File.new()
-	var erro = arquivo.open(SAVE_PATH, File.WRITE)
-	var dados = {"contador" : contador}
+	var arquivo = File.new() # Cria a instância do arquivo
+	var erro = arquivo.open(SAVE_PATH, File.WRITE) # Abre o arquivo para escrita
+	var dados = {"contador" : contador} # Dicionário para armazenar os dados a serem salvos
 	if not erro:
-		arquivo.store_var(dados)
+		arquivo.store_var(dados) # Salva os dados no arquivo
 	else:
 		print("O contador não pode ser salvo.")
-	arquivo.close()
+	arquivo.close() # Fecha o arquivo
 
+# Método de carregamento do contador 
 func _load():
-	var arquivo = File.new()
-	if arquivo.file_exists(SAVE_PATH):
-		arquivo.open(SAVE_PATH, File.READ)
-		var dados_salvos = arquivo.get_var()
-		contador = dados_salvos["contador"]
+	var arquivo = File.new() # Cria a instância do arquivo
+	if arquivo.file_exists(SAVE_PATH): # Verifica se o arquivo existe
+		arquivo.open(SAVE_PATH, File.READ) # Abre o arquivo para leitura
+		var dados_salvos = arquivo.get_var() # recupera os dados do arquivo e seta esses dados para a variável dados_salvos
+		contador = dados_salvos["contador"] # Atualiza com os dados recuperados o contador
 	else:
 		print("Erro ao carregar os dados do contador")
